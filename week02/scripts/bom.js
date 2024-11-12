113c27fceb70c3e3b3fb2ffe2b807bd960dd5ac4
cindyjanevick <calarco@byui.edu>
2024-11-12 10:24:28 +0900

    bom2

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,10 +2,24 @@ const list = document.querySelector('#list');
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 
-const li = document.createElement('li');
+button.addEventListener('click',function() {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        li.textContent = input.value;
+        
+        const deleteButton = document.createElement('button');
+        
+        deleteButton.textContent = '❌'; 
+        li.appendChild(deleteButton);
+        list.appendChild(li);
+    
+        deleteButton.addEventListener('click',function(){
+            list.removeChild(li);
+            input.focus();
+        });
+        input.value('');
+    }
+    input.focus('');
 
-const deleteButton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = '❌'; 
-li.append(deleteButton);
-list.append(li);
+});
+    
