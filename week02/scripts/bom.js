1a70110f0cc0ce326c760b414e7321febd95c45b
cindyjanevick <calarco@byui.edu>
2024-11-12 10:07:19 +0900

    bom1

---
+++ week02/scripts/bom.js
@@ -0,0 +1,11 @@
+const list = document.querySelector('#list');
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+
+const li = document.createElement('li');
+
+const deleteButton = document.createElement('button');
+li.textContent = input.value;
+deleteButton.textContent = '❌'; 
+li.append(deleteButton);
+list.append(li);
