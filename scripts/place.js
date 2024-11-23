0336e7058e96dcc3ca0f3fafb1d544bd0c1ca665
cindyjanevick <calarco@byui.edu>
2024-11-24 01:41:04 +0900

    comments

--- scripts/place.js
+++ scripts/place.js
@@ -2,11 +2,11 @@
 and Last Modified*/
 
 const currentYear = document.getElementById('currentyear');
-const today = new Date(); // Getting the current year and date
+const today = new Date(); 
 
-currentYear.textContent = `${today.getFullYear()}`;  // Add the current year to the HTML document
+currentYear.textContent = `${today.getFullYear()}`; 
 
-// dynamically show the last date modified
+
 let dateLastModified = document.lastModified;
 let displayContent = document.getElementById('lastmodified');
 
@@ -19,9 +19,9 @@ displayContent.textContent = `Last Modification: ${dateLastModified}`;
 
 function calculateWindChill(temperature, windSpeed) {
     if (temperature <= 10 && windSpeed > 4.8) {
-        // Wind chill calculation formula
+        
         const windChill = 13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperature * Math.pow(windSpeed, 0.16);
-        return windChill.toFixed(2); // Returning the wind chill rounded to two decimal places
+        return windChill.toFixed(2); 
     } else {
         return "N/A";
     }
