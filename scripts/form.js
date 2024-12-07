19bf4b08fff4970479971e112dc7d0900e44dccb
cindyjanevick <calarco@byui.edu>
2024-12-08 02:56:41 +0900

    1`

--- scripts/form.js
+++ scripts/form.js
@@ -1,32 +1,23 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const currentYearSpan = document.getElementById("currentyear");
-    const lastModifiedSpan = document.getElementById("lastModified");
-
-    const currentYear = new Date().getFullYear();
-    currentYearSpan.textContent = currentYear;
-
-    lastModifiedSpan.textContent = "Last Modification: " + document.lastModified;
-});
-
-
 document.addEventListener('DOMContentLoaded', function() {
-    const productSelect = document.getElementById('product-select'); // Reference to the <select> element
+    const checkboxes = document.querySelectorAll('input[name="Feature"]');
+    const form = document.querySelector('form');
+    const productSelect = document.getElementById('product-select');
 
-const products = [
-    { id: 1, name: "Baby Onesie - Newborn" },
-    { id: 2, name: "Baby Romper - 3-6 Months" },
-    { id: 3, name: "Baby Blanket - Soft Cotton" },
-    { id: 4, name: "Baby Hat - Wool Knit" },
-    { id: 5, name: "Baby Shoes - 6-12 Months" }
-];
+    const products = [
+        { id: 1, name: "Baby Onesie - Newborn" },
+        { id: 2, name: "Baby Romper - 3-6 Months" },
+        { id: 3, name: "Baby Blanket - Soft Cotton" },
+        { id: 4, name: "Baby Hat - Wool Knit" },
+        { id: 5, name: "Baby Shoes - 6-12 Months" }
+    ];
 
-
-  products.forEach(product => {
+    products.forEach(product => {
         const option = document.createElement('option');
         option.value = product.id;  // Set the product ID as the value of the option
         option.textContent = product.name;  // Set the product name as the display text
         productSelect.appendChild(option);  // Append the option to the <select> element
     });
+
     form.addEventListener('submit', function(event) {
         let checkedOne = Array.prototype.slice.call(checkboxes).some(x => x.checked);
         if (!checkedOne) {
@@ -43,6 +34,15 @@ const products = [
         }
     });
 
+    document.addEventListener("DOMContentLoaded", function() {
+        const currentYearSpan = document.getElementById("currentyear");
+        const lastModifiedSpan = document.getElementById("lastModified");
     
+        const currentYear = new Date().getFullYear();
+        currentYearSpan.textContent = currentYear;
+    
+        
+        lastModifiedSpan.textContent = "Last Modification: " + document.lastModified;
+    });
     
 })
