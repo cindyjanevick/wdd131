a757cc1e58c065174ea3f4268e513c951d2a5f7c
cindyjanevick <calarco@byui.edu>
2024-12-08 13:28:14 +0900

    2

--- scripts/form.js
+++ scripts/form.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
   const products = [
-      { id: 'fc-1888', name: "flux capacitor", avg_rating: 4.5 },
-      { id: 'fc-2050', name: "power laces", avg_rating: 4.7 },
-      { id: 'fs-1987', name: "time circuits", avg_rating: 3.5 },
-      { id: 'ac-2000', name: "low voltage reactor", avg_rating: 3.9 },
-      { id: 'jj-1969', name: "warp equalizer", avg_rating: 5.0 }
+      { id: 'onesie', name: "Baby Onesie - Newborn", avg_rating: 4.5 },
+      { id: 'romper', name: "Baby Romper - 3-6 months", avg_rating: 4.7 },
+      { id: 'blanket', name: "Baby blanket - soft cotton", avg_rating: 3.5 },
+      { id: 'hat', name: "Baby Hat - Wool Knit", avg_rating: 3.9 },
+      { id: 'shoes', name: "Baby shoes -  6-12 months", avg_rating: 5.0 }
   ];
 
   const selectElement = document.getElementById('product-name');
